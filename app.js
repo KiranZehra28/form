daa41cad101d367838721fbea229b00a5bbf23b0
Kiran Zehra <103608035+KiranZehra28@users.noreply.github.com>
2022-05-26 05:02:31 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+$('h1').after('<h1>World</h1>');
+$('p').text('hsdhnjhjbjk')
+
+$('.show').hover(function(){
+    $('div').show(1000);
+})
+
+$('.hide').hover(function(){
+    $('div').hide(1000);
+})
+
+$('.toggle').hover(function(){
+    $('div').toggle(1000);
+})
